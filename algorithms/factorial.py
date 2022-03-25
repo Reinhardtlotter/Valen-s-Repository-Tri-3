67a710857897a335decc83c3c71990c4f9be0cc1
Greg Reynolds <gregoryareynoldsjr@gmail.com>
2022-03-25 14:49:11 -0700

    Add more unit tests

--- algorithms/factorial.py
+++ algorithms/factorial.py
@@ -1,17 +1,20 @@
-
+import math
 # Python code to demonstrate naive method
 # to compute factorial
 
 class Factor:
 
     def factorial(n):
+        if n < 0:
+            return math.nan
+
         fact = 1
-        
         for i in range(1,n+1):
             fact = fact * i
             
-        print ("The factorial of " , n, " is : ",end="")
+        print ("The factorial of " , n, " is : ")
         print (fact)
+        return fact
 
     # Test Code
 if __name__ == "__main__":
--- algorithms/factorial_test.py
+++ algorithms/factorial_test.py
@@ -1,5 +1,13 @@
-from factorial import factorial
+from unittest import TestCase
+from factorial import Factor
+import math
 
-def test_factorial_5():
-    result = factorial(5)
-    assert result == 120
+class TestFactorial(TestCase):
+    def test_factorials (self):
+        self.assertEqual (Factor.factorial(5), 120)
+        self.assertEqual (Factor.factorial(4), 24)
+        self.assertEqual (Factor.factorial(0), 1)
+        self.assertEqual (Factor.factorial(1), 1)
+ 
+    def test_negative_factor (self):
+        self.assertTrue(math.isnan(Factor.factorial(-1)))
